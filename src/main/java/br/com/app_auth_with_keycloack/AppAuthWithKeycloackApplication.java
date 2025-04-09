29f24f4cd5322e4cd655ff14c5445cd371d8a529
carlos <carlosjuvenal.sysmap@natura.net>
2025-04-09 11:36:43 -0300

    chore: initial project

---
+++ src/main/java/br/com/app_auth_with_keycloack/AppAuthWithKeycloackApplication.java
@@ -0,0 +1,13 @@
+package br.com.app_auth_with_keycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAuthWithKeycloackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAuthWithKeycloackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/app_auth_with_keycloack/AppAuthWithKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app_auth_with_keycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAuthWithKeycloackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
